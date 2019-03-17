a760d91e234db53dd032c9996d0d7aca5cc15927
Adriana <adrirx5@gmail.com>
2019-03-17 23:33:03 +0100

    iteration 3 working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import food from './data/foods.json';
+import FoodBox from './components/FoodBox.js';
+import AddFood from './components/AddFood.js';
 
-class App extends Component {
+class App extends Component {  
+  state = {
+    data: food
+  }
+
+  addFoodHandler = (theFood) => {    
+    const foodCopy = [...this.state.data];
+    foodCopy.push(theFood);
+    this.setState({
+      data: foodCopy
+    })
+  }
+
   render() {
+    const items = this.state.data.map((food, index) => 
+      <FoodBox key={index} food={food}></FoodBox>
+    )
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Iron Nutrition</h1>
+        <AddFood addTheFood={this.addFoodHandler}></AddFood>
+        <input type="text" className="input" placeholder="Search..." />
+        {items}
       </div>
     );
   }
